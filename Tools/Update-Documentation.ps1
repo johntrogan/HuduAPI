7c251eb698b2d11d1e324251bd4277b11b14b925
masonville17 <anoniboni9876@gmail.com>
2026-03-23 16:18:38 -0400

    including previous docs method, comented

--- Tools/Update-Documentation.ps1
+++ Tools/Update-Documentation.ps1
@@ -1,3 +1,8 @@
+# Build-Module
+# Remove-Module HuduAPI
+# Import-Module .\Output\HuduAPI\HuduAPI.psd1
+# New-MarkdownHelp -Module HuduAPI -OutputFolder .\Docs\ -Force
+
 remove-module huduapi -Force
 IMPORT-MODULE C:\Users\Administrator\Documents\GitHub\HuduAPI\HuduAPI\HuduAPI.psd1
 
